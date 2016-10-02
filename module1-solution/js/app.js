12bc5bd35189ad90b5d996976c6171d51690bbe4
Vitalii Maklai <vitalii.maklai@gmail.com>
2016-10-01 20:49:01 -0400

    First working version of Module 1 Coding Assignment

---
+++ module1-solution/js/app.js
@@ -0,0 +1,41 @@
+﻿(function ()
+{
+    'use strict';
+
+    angular.module('LunchCheck', [])
+        .controller('LunchCheckController', LunchCheckController);
+
+    function isNullOrWhitespace(str)
+    {
+      return !str || !/\S/.test(str);
+    }
+
+    LunchCheckController.$inject = ['$scope'];
+    function LunchCheckController($scope)
+    {
+        $scope.dishes = '';
+        $scope.message = '';
+
+        $scope.onCheckButtonClicked = function ()
+        {
+            var dishesString = $scope.dishes || '';
+            if (isNullOrWhitespace(dishesString))
+            {
+                $scope.message = 'Please enter data first';
+                return;
+            }
+
+            var dishes = dishesString.split(',');
+            var dishCount = 0;
+            for (var i = 0; i < dishes.length; i++)
+            {
+                if (!isNullOrWhitespace(dishes[i]))
+                {
+                    dishCount++;
+                }
+            }
+
+            $scope.message = dishCount <= 3 ? "Enjoy!" : "Too much!";
+        };
+    }
+})();
