eda5f7611cb5c02a56fad786927eefba43ca2682
Vitalii Maklai <vitalii.maklai@gmail.com>
2016-10-16 14:56:49 -0400

    Module 3 Solution: First working version

--- module3-solution/js/app.js
+++ module3-solution/js/app.js
@@ -2,7 +2,112 @@
 {
     'use strict';
 
-    //angular.module('...', [])
-    //    .service('...', ...)
-    //    .controller('...', ...);
+    angular.module('NarrowItDownApp', [])
+        .service('MenuSearchService', MenuSearchService)
+        .controller('NarrowItDownController', NarrowItDownController)
+        .directive('foundItems', FoundItemsDirective);
+
+    function isNullOrWhitespace(str)
+    {
+      return !str || !/\S/.test(str);
+    }
+
+    MenuSearchService.$inject = ['$http'];
+    function MenuSearchService($http)
+    {
+        var service = this;
+
+        function normalizeString(value)
+        {
+            return value.toLowerCase();
+        }
+
+        service.getMatchedMenuItems = function (searchTerm)
+        {
+            var searchTermNormalized = normalizeString(searchTerm);
+
+            return $http
+                .get('https://davids-restaurant.herokuapp.com/menu_items.json')
+                .then(
+                    function (response)
+                    {
+                        var items = response.data.menu_items;
+                        if (!angular.isArray(items))
+                        {
+                            return [];
+                        }
+
+                        return items.filter(
+                            function (item)
+                            {
+                                return normalizeString(item.description).indexOf(searchTermNormalized) >= 0;
+                            });
+                    });
+        };
+    }
+
+    NarrowItDownController.$inject = ['MenuSearchService'];
+    function NarrowItDownController(MenuSearchService)
+    {
+        var controller = this;
+
+        controller.found = [];
+        controller.searchTerm = '';
+
+        controller.search = function ()
+        {
+            controller.found = [];
+            if (isNullOrWhitespace(controller.searchTerm))
+            {
+                return;
+            }
+
+            MenuSearchService
+                .getMatchedMenuItems(controller.searchTerm)
+                .then(
+                    function (result)
+                    {
+                        var found = result;
+                        found.sort(function (l, r) { return l.name.localeCompare(r.name); })
+                        controller.found = found;
+                    });
+        };
+
+        controller.removeItem = function (itemIndex)
+        {
+            controller.found.splice(itemIndex, 1);
+        };
+    }
+
+    function FoundItemsDirective()
+    {
+        var ddo =
+        {
+            templateUrl: 'foundItems/foundItems.template.html',
+            restrict: 'E',
+            scope:
+            {
+                items: '<foundItems',
+                onRemove: '&'
+            },
+            controller: FoundItemsDirectiveController,
+            controllerAs: 'directiveController',
+            bindToController: true
+        };
+
+        return ddo;
+    }
+
+    FoundItemsDirectiveController.$inject = [];
+    function FoundItemsDirectiveController()
+    {
+        var controller = this;
+
+        controller.hasItems = function ()
+        {
+            var items = controller.items;
+            return items && items.length && items.length >= 0;
+        };
+
+    }
 })();
